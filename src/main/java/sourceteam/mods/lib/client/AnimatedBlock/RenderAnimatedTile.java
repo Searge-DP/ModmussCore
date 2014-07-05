81c666bac241e7c8941b850552e6773560194bc4
modmuss50 <m123world@gmail.com>
2014-07-05 16:53:43 +0100

    @toby have a look at this

--- src/main/java/sourceteam/mods/lib/client/AnimatedBlock/RenderAnimatedTile.java
+++ src/main/java/sourceteam/mods/lib/client/AnimatedBlock/RenderAnimatedTile.java
@@ -1,30 +1,20 @@
 package sourceteam.mods.lib.client.AnimatedBlock;
 
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.StatCollector;
-import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.FluidStack;
 import org.lwjgl.opengl.GL11;
 import sourceteam.mods.core.Core;
-import sourceteam.mods.core.fluid.BlockFluid;
 import sourceteam.mods.lib.client.IColour;
 
 public class RenderAnimatedTile extends TileEntitySpecialRenderer {
 
 
-    public static final Fluid fluid = new Fluid("fluidBlank") {
-        @Override
-        public String getLocalizedName() {
-            return StatCollector.translateToLocal("tile.fluidBlank.name");
-        }
-    }.setBlock(Blocks.water).setUnlocalizedName(Blocks.water.getUnlocalizedName()).setIcons(BlockFluid.StillIcon);
 
     @Override
     public void renderTileEntityAt(TileEntity tileentity, double x, double y, double z, float f) {
         FluidStack liquid = new FluidStack(Core.blankFluid, 100);
-     //   liquid = new FluidStack(fluid, 100);
+      //  liquid = new FluidStack(FluidRegistry.LAVA, 100);
         int color = 0xFFFFFF;
         if (liquid == null || liquid.amount <= 0) {
             return;
@@ -42,14 +32,21 @@ public class RenderAnimatedTile extends TileEntitySpecialRenderer {
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
-        bindTexture(FluidRenderer.getFluidSheet(liquid));
-       // bindTexture(new ResourceLocation("sourcecore", "textures/blocks/fluid.png"));
+         bindTexture(FluidRenderer.getFluidSheet(liquid));
+      //  bindTexture(new ResourceLocation("sourcecore", "textures/blocks/fluid.png"));
+
+        setGLColorFromInt(0x666666);
 
         if(tileentity instanceof IColour){
-            GL11.glColor4f((float)((IColour)tileentity).red(), (float)((IColour)tileentity).green(), (float)((IColour)tileentity).blue(), 1.0F);
+           setGLColorFromInt(((IColour) tileentity).colour());
         }
 
+    //   RenderUtils.renderFluidQuad(new Vector3(x + 2, 2 + y, 10), new Vector3(x + 2, 8 + y, 10), new Vector3(x + 2 + 16, 8 + y, 16), new Vector3(x + 2 + 16, 2 + y, 10), BlockFluid.StillIcon , 16D);
 
+//        RenderUtils.renderFluidCuboid(liquid,
+//                new Cuboid6(0.22, 0.12, 0.22, 0.78, 0.121+0.63, 0.78)
+//                        .add(new Vector3(x, y, z)),
+//                1000/(16D* FluidUtils.B), 0.75);
 
         GL11.glTranslatef((float) x + 0.001F, (float) y + 0.5F, (float) z + 0.001F);
         GL11.glScalef(0.999F, 0.999F, 0.999F);
@@ -57,8 +54,21 @@ public class RenderAnimatedTile extends TileEntitySpecialRenderer {
 
         GL11.glCallList(displayList[(int) ((float) 100 / (float) (100) * (FluidRenderer.DISPLAY_STAGES - 1))]);
 
+
+//        CCRenderState.reset();
+//        CCRenderState.pullLightmap();
+//        //   CCRenderState.useNormals(true);
+//
+//        RenderUtils.renderFluidCuboid(liquid,
+//                new Cuboid6(0.01, 0.01, 0.01, 0.99, 0.121+0.63, 0.99)
+//                        .add(new Vector3(x, y, z)),
+//                1300/(1D* FluidUtils.B), 1);
+
+
         GL11.glPopAttrib();
         GL11.glPopMatrix();
+
+
     }
 
     public static void setGLColorFromInt(int color) {
--- src/main/java/sourceteam/mods/lib/client/IColour.java
+++ src/main/java/sourceteam/mods/lib/client/IColour.java
@@ -2,8 +2,6 @@ package sourceteam.mods.lib.client;
 
 public interface IColour {
 
-    public int red();
-    public int blue();
-    public int green();
+    public int colour();
 
 }
