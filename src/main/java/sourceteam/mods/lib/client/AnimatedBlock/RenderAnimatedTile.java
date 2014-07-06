079e54394660804fb0c1310f42fdaf048c0668e1
modmuss50 <m123world@gmail.com>
2014-07-06 13:03:37 +0100

    clened up

--- src/main/java/sourceteam/mods/lib/client/AnimatedBlock/RenderAnimatedTile.java
+++ src/main/java/sourceteam/mods/lib/client/AnimatedBlock/RenderAnimatedTile.java
@@ -9,37 +9,28 @@ import sourceteam.mods.lib.client.IColour;
 
 public class RenderAnimatedTile extends TileEntitySpecialRenderer {
 
-
-
     @Override
     public void renderTileEntityAt(TileEntity tileentity, double x, double y, double z, float f) {
         FluidStack liquid = new FluidStack(Core.blankFluid, 100);
-
         if(tileentity instanceof IHighRes && ((IHighRes) tileentity).useHighRes()){
             liquid = new FluidStack(Core.blankFluidH, 100);
         }
-
-
         int color = 0xFFFFFF;
         if (liquid == null || liquid.amount <= 0) {
             return;
         }
-
         int[] displayList = FluidRenderer.getFluidDisplayLists(liquid, tileentity.getWorldObj(), false);
         if (displayList == null) {
             return;
         }
-
         GL11.glPushMatrix();
         GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
         GL11.glEnable(GL11.GL_CULL_FACE);
         GL11.glDisable(GL11.GL_LIGHTING);
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-
-         bindTexture(FluidRenderer.getFluidSheet(liquid));
+        bindTexture(FluidRenderer.getFluidSheet(liquid));
         setGLColorFromInt(0x666666);
-
         if(tileentity instanceof IColour){
            setGLColorFromInt(((IColour) tileentity).colour());
         }
@@ -50,8 +41,6 @@ public class RenderAnimatedTile extends TileEntitySpecialRenderer {
         GL11.glCallList(displayList[(int) ((float) 100 / (float) (100) * (FluidRenderer.DISPLAY_STAGES - 1))]);
         GL11.glPopAttrib();
         GL11.glPopMatrix();
-
-
     }
 
     public static void setGLColorFromInt(int color) {
