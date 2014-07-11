cffb762db4bfe04a9aae57ba15308736255a0239
modmuss50 <m123world@gmail.com>
2014-07-11 14:58:24 +0100

    Started work on threaded Schematics and Core mods

---
+++ src/main/java/sourceteam/mods/core/Transformer.java
@@ -0,0 +1,26 @@
+package sourceteam.mods.core;
+
+import net.minecraft.launchwrapper.IClassTransformer;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.VarInsnNode;
+
+import cpw.mods.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
+
+public class Transformer implements IClassTransformer {
+
+	@Override
+	public byte[] transform(String name, String decodedName, byte[] code) {
+		return code;
+	}
+	
+
+}
