632029a754d220a6bddfe396df935fe00c2bbd92
modmuss50 <m123world@gmail.com>
2014-09-10 16:11:53 +0100

    some work on stuffs(Untested)

--- src/main/java/sourceteam/mods/core/Core.java
+++ src/main/java/sourceteam/mods/core/Core.java
@@ -33,7 +33,7 @@ public class Core implements ISourceMod {
 
     public static final String MODID = "sourcecore";
     public static final String NAME = "Source Core";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "@MODVERSION@";
 
     public ModLogger logger = ModLogger.getLogger(NAME);
 
