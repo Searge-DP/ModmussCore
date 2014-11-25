2c1510d12571867c3ad21494f99bfa7613762c5f
modmuss50 <m123world@gmail.com>
2014-11-25 18:29:42 +0000

    added copy right

--- src/main/java/me/modmuss50/mods/core/client/ClientInit.java
+++ src/main/java/me/modmuss50/mods/core/client/ClientInit.java
@@ -1,3 +1,7 @@
+/*
+ * This file was made by modmuss50. View the licence file to see what licence this is is on. You can always ask me if you would like to use part or all of this file in your project.
+ */
+
 package me.modmuss50.mods.core.client;
 
 import cpw.mods.fml.client.registry.RenderingRegistry;
--- src/main/java/me/modmuss50/mods/lib/multiblock/MultiblockWorldRegistry.java
+++ src/main/java/me/modmuss50/mods/lib/multiblock/MultiblockWorldRegistry.java
@@ -1,3 +1,7 @@
+/*
+ * This file was made by modmuss50. View the licence file to see what licence this is is on. You can always ask me if you would like to use part or all of this file in your project.
+ */
+
 package me.modmuss50.mods.lib.multiblock;
 
 import me.modmuss50.mods.lib.BeefCoreLog;
